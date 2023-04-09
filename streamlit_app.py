a77081a3b1a88002e5054a98d0ecb4daffd0ceb7
mparje <126131806+mparje@users.noreply.github.com>
2023-04-09 03:43:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,43 @@
+import openai
+import os
 import streamlit as st
 from streamlit_chat import Chat
 from googlesearch import search
 
-# Función para buscar sitios web de Streamlit
+# Cargar la clave API de OpenAI
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 def search_streamlit_apps(query):
     results = []
     for url in search(query + " site:*.streamlit.app", num_results=10):
         results.append(url)
     return results
 
-# Función para procesar los mensajes del usuario
+def get_gpt3_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    return response.choices[0].text.strip()
+
 def process_message(message):
-    results = search_streamlit_apps(message)
-    if results:
-        response = "Resultados de la búsqueda:\n" + "\n".join(results)
+    if message.lower().startswith("buscar:"):
+        query = message[7:].strip()
+        results = search_streamlit_apps(query)
+        if results:
+            response = "Resultados de la búsqueda:\n" + "\n".join(results)
+        else:
+            response = "No se encontraron resultados."
     else:
-        response = "No se encontraron resultados."
+        prompt = f"Usuario: {message}\nChatbot:"
+        response = get_gpt3_response(prompt)
     return response
 
-# Inicializar el chat
 chat = Chat(process_message)
 
-# Configurar la aplicación de Streamlit
-st.title("Chatbot buscador de sitios *.streamlit.app")
+st.title("Chatbot combinado: buscador de sitios *.streamlit.app y GPT-3")
 chat.render()
